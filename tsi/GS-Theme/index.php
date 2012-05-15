42e601eaffe5ad7f47433a141f76228f4031f93c
Oren <get.oren@gmail.com>
2012-05-15 13:54:00 -0300

    Formatted to use inline HTML/PHP & standardizes spacing/formatting.

--- tsi/GS-Theme/index.php
+++ tsi/GS-Theme/index.php
@@ -1,42 +1,34 @@
-<?php
-get_header();
-$DIV_ID	=	"'body_left'";
-echo
-	"<div	id	=	$DIV_ID	>";
-if (have_posts()) :
-   while (have_posts()) :
-      the_post();
-      the_content();
-   endwhile;
-endif;
+<?php get_header(); ?>
 
-$DIV_ID	=	"'body_center'";
-echo
-	"</div>
-	<div	id	=	$DIV_ID	>";
-if	(	true	==	have_posts()
-	)
-{	while	(	true	==	have_posts()
-   &&			true	==	is_tag(	'body_center'	)
-   		)
-   {	the_post();
-		the_content();
-}	}
+<div id="body_left">';
+  <?php if (have_posts() === TRUE):?>
+    <?php
+      while (have_posts()) {
+        the_post();
+        the_content();
+      }
+    ?>
+  <?php endif; ?>
+</div>
+<div id="body_center">
+  <?php if (have_posts()): ?>
+    <?php
+      while (have_posts() && is_tag('body_center')) {
+        the_post();
+        the_content();
+      }
+    ?>
+  <?php endif; ?>
+</div>
+<div id="body_right">
+  <?php if (have_posts()): ?>
+    <?php
+      while (have_posts() && is_tag('body_right')) {
+        the_post();
+        the_content();
+      }
+    ?>
+  <?php endif; ?>
+</div>
 
-$DIV_ID	=	"'body_right'";
-echo
-	"</div>
-	<div	id	=	$DIV_ID	>";
-if	(	true	==	have_posts()	)
-{	while	(	true	==	have_posts()
-   &&			true	==	is_tag(	'body_right'	)
-   		)
-   {	the_post();
-		the_content();
-}	}
-	
-echo
-	"</div>";
-   
-get_footer();
-?>
+<?php get_footer(); ?>
